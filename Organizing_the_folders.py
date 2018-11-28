3e650614c06b973cc13a53398ecc90e86452553e
lalithchandu <lchandu1@lsu.edu>
2018-11-28 13:10:55 -0600

    Zip folder for the collection is generated

--- Organizing_the_folders.py
+++ Organizing_the_folders.py
@@ -4,7 +4,9 @@ import re
 import shutil
 from zipfile import ZipFile
 path = 'C:\\Users\\libsys\\Desktop\\success'
-target = 'C:\\Users\\libsys\\Desktop\\success'
+target = 'C:\\Users\\libsys\\Desktop\\test'
+#shutil.copytree(path,target)
+collections = []
 main1 = os.listdir(path)
 for each in main1:
     main_fol = os.path.join(path,each)
@@ -16,6 +18,7 @@ for each in main1:
         new_path = os.path.join(target,plain)
         if not os.path.exists(new_path):
             os.mkdir(new_path)
+            collections.append(plain)
         sub_fol_new = os.path.join(new_path,single)
         if not os.path.exists(sub_fol_new):
             os.mkdir(sub_fol_new)
@@ -25,40 +28,32 @@ for each in main1:
                 pdf_path = os.path.join(sub_fol_new,'PDF.pdf')
                 shutil.copy(file_path,pdf_path)
             if file.endswith('.xml'):
-                n = len(file)
-                xml = file[n-7:n-4]
-                if xml == "ETS":
+                if file.find("METS"):
                     xml_path = os.path.join(sub_fol_new,'MODS.xml')
                     shutil.copy(file_path,xml_path)
+                    #add xslt stuff eventually...
             if file.endswith('.jp2'):
-                l = len(file)
-                jp = file[l-7:l-4]
-                new_fol = os.path.join(sub_fol_new,jp)
+                jp = re.search('\d+(?=\.\w+$)', file)
+                new_fol = os.path.join(sub_fol_new,jp.group(0))
                 if not os.path.exists(new_fol):
-                    os.mkdir(new_fol)
+                    os.mkdir(new_fol) 
                 jp_path = os.path.join(new_fol,'OBJ.jp2')
                 shutil.copy(file_path,jp_path)
             if file.endswith('.txt'):
-                m = len(file)
-                txt = file[m-7:m-4]
-                new_fol = os.path.join(sub_fol_new,txt)
+                txt = re.search('\d+(?=\.\w+$)', file)
+                new_fol = os.path.join(sub_fol_new,txt.group(0))
                 if not os.path.exists(new_fol):
                     os.mkdir(new_fol)
                 txt_path = os.path.join(new_fol,'OCR.txt')
                 shutil.copy(file_path,txt_path)
-    shutil.rmtree(main_fol)
-file_paths = []
-zp = os.path.join(plain,'.zip')
-for folder, subfolders, files_1 in os.walk(new_path): 
-        for eachfilename in files_1: 
-            # join the two strings in order to form the full filepath. 
-            filepath = os.path.join(folder, eachfilename) 
-            file_paths.append(filepath) 
-for file_name in file_paths: 
-        print(file_name) 
-    # writing files to a zipfile 
-with ZipFile(zp,'w') as zip: 
-        # writing each file one by one 
-        for file in file_paths: 
-            zip.write(file) 
-
+#    shutil.rmtree(main_fol)
+os.chdir(target)
+for collection in collections:
+    zp = collection + '.zip'
+    zipf = ZipFile(zp , mode='w')
+    lenDirPath = len(new_path)
+    for root, _ , files in os.walk(new_path):
+        for file in files:
+            filePath = os.path.join(root, file)
+            zipf.write(filePath , filePath[lenDirPath :] )
+    zipf.close()
